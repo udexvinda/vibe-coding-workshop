3b54bc088f8e7023fb3e0981440108b84b408b1e
Yudesh Govindaraju <yudesh.g@gmail.com>
2025-12-11 10:02:00 +0800

    Update HeroSection.tsx

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -45,11 +45,11 @@ const HeroSection: React.FC = () => {
           <div className="flex flex-col sm:flex-row justify-center items-center gap-x-6 gap-y-2 text-stone-700 font-medium">
             <div className="flex items-center gap-2">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2"><path strokeLinecap="round" strokeLinejoin="round" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" /></svg>
-              <span>December 13, 2025 (Saturday)</span>
+              <span>Postponed (TBD)</span>
             </div>
             <div className="flex items-center gap-2">
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2"><path strokeLinecap="round" strokeLinejoin="round" d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" /><path strokeLinecap="round" strokeLinejoin="round" d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" /></svg>
-              <span>Sunway Geo Avenue, PJ</span>
+              <span>TBD</span>
             </div>
           </div>
         </div>
